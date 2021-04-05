f593505fb751d57623096a6be5f20525b7342e68
Armand <ag9217@ic.ac.uk>
2021-04-05 23:45:32 +0200

    added D and I controller

--- PID.py
+++ PID.py
@@ -1,32 +1,46 @@
 import numpy as np
 import math
 
-def PID_control(robot_action, robot_target_diff):
-    # PID Gains
-    K_p = 1
-    K_I = 0.1
-    K_D = 0.1
+class PID:
 
-    # Creating unit vectors
-    robot_action_unit = robot_action / np.linalg.norm(robot_action)
-    robot_target_diff_unit = robot_target_diff / np.linalg.norm(robot_target_diff)
+    def __init__(self, Kp, Ki, Kd):
+        # PID Gains
+        self.K_p = Kp
+        self.K_i = Ki
+        self.K_d = Kd
 
-    # Computing dot product
-    dot_product = np.dot(robot_action_unit, robot_target_diff_unit)
-    # Computing angle between unit vectors based on dot product (in degrees)
-    error_angle = np.arccos(dot_product)
+        # Memory for I and D controllers
+        self.prev_angle = 0
+        self.error_sum = 0
 
-    # Condition when robot should turn left
-    if robot_action_unit[0] < robot_target_diff_unit[0] or robot_action_unit[1] > robot_target_diff_unit[1]:
-        error_angle = -error_angle
+    def PID_control(self, robot_action, robot_target_diff):
 
-    # Calculating PID output
-    PID_h = K_p * error_angle
+        # Creating unit vectors
+        robot_action_unit = robot_action / np.linalg.norm(robot_action)
+        robot_target_diff_unit = robot_target_diff / np.linalg.norm(robot_target_diff)
 
-    # Creating rotation matrix to rotate robot action
-    rot = np.array([[math.cos(PID_h), -math.sin(PID_h)], [math.sin(PID_h), math.cos(PID_h)]])
+        # Computing dot product
+        dot_product = np.dot(robot_action_unit, robot_target_diff_unit)
+        # Computing angle between unit vectors based on dot product (in degrees)
+        error_angle = np.arccos(dot_product)
 
-    # Rotating robot action vector
-    robot_new_action = np.dot(rot,robot_action)
+        # Saving error for integral term
+        self.error_sum += error_angle
 
-    return robot_new_action
+        # Condition when robot should turn left
+        if robot_action_unit[0] < robot_target_diff_unit[0] or robot_action_unit[1] > robot_target_diff_unit[1]:
+            error_angle = -error_angle
+
+        # Calculating PID output
+        PID_h = self.K_p * error_angle + self.K_i * self.error_sum + self.K_d * (error_angle - self.prev_angle)
+        print(PID_h)
+
+        # Creating rotation matrix to rotate robot action
+        rot = np.array([[math.cos(PID_h), -math.sin(PID_h)], [math.sin(PID_h), math.cos(PID_h)]])
+
+        # Rotating robot action vector
+        robot_new_action = np.dot(rot,robot_action)
+
+        self.prev_angle = error_angle
+
+        return robot_new_action
