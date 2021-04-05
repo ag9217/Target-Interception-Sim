6e9239f26717b15b2045515c79d1be2f0640d072
Armand <ag9217@ic.ac.uk>
2021-04-05 20:59:56 +0200

    added line to have robot actions depend on PID output (currently only P)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 import cv2
+from PID import PID_control
 
 from environment import Environment
 
@@ -12,13 +13,16 @@ robot_state = environment.init_state
 target_state = environment.target_init_state
 
 # Robot action (will be determined by PID controller)
-action = np.array([0.1,0.1])
+action = np.array([0.25,0.25])
 
 # Variable when simulation is finished
 sim_done = False
 
 while not sim_done:
-    robot_state, target_state, target_action, dist, sim_done = environment.step(robot_state, action, target_state)
+    robot_state, target_state, target_action, diff_vector, sim_done = environment.step(robot_state, action, target_state)
     environment.show(robot_state, target_state)
+
+    # Calculating robot action based off PID
+    action = PID_control(action, diff_vector)
 
 cv2.waitKey(0)
