d4523f46a78152593e53f2e8d5d2473f2833f68c
Armand <ag9217@ic.ac.uk>
2021-04-21 16:16:39 +0200

    added delay function and added matplotlib as module

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 from PID import PID
 from PN import PN
 
@@ -8,6 +9,9 @@ from environment import Environment
 
 # Create an environment
 environment = Environment(magnification=3, target_speed=0.2)
+
+# Delay variable
+delay = 5
 
 # Creating controllers
 pid = PID(0.7, 0.0, 0.60)
@@ -17,23 +21,38 @@ pn = PN(3)
 robot_state = environment.init_state
 target_state = environment.target_init_state
 
-# Robot action (will be determined by PID controller)
+# Initial robot action (will be determined by PID controller)
 action = np.array([0.3,0.3])
+
+# Action history (used to simulate delay in discrete steps)
+actions = list()
+for i in range(1,delay):
+    actions.append([0, 0])
+
+# Appending initial action
+actions.append(action)
 
 # Variable when simulation is finished
 sim_done = False
 
+# Counter during simulation
+count = 0
+
 while not sim_done:
-    robot_state, target_state, target_action, diff_vector, sim_done = environment.step(robot_state, action, target_state)
+    robot_state, target_state, target_action, diff_vector, sim_done = environment.step(robot_state, actions[count], target_state)
     environment.show(robot_state, target_state)
 
-    if np.linalg.norm(robot_state - target_state) < 0.1:
+    if np.linalg.norm(robot_state - target_state) < 0.5:
         sim_done = True
 
     # Calculating robot action based off PID
-    #action = pid.PID_control(action, diff_vector)
+    action = pid.PID_control(action, diff_vector)
 
     # Calculating robot action based off PN
-    action = pn.PN_control(action, diff_vector)
+    #action = pn.PN_control(action, diff_vector)
+
+    actions.append(action)
+
+    count += 1
 
 cv2.waitKey(0)
