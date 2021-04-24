04756dee5f8cef261827d3a0a89298ce5ac61b93
Armand <ag9217@ic.ac.uk>
2021-04-24 14:31:31 +0200

    added time steps

--- PN.py
+++ PN.py
@@ -1,37 +1,52 @@
 # Proportional navigation class
 import numpy as np
 import math
+import time
 
 class PN:
 
-    def __init__(self,gain):
+    def __init__(self,gain,s_time):
         # Variable to keep previous diff vector
         self.robot_target_diff_old = np.array([-79.8, 119.75])
 
         # Gain
         self.N = gain
 
+        # Times
+        self.current_time = time.time()
+        self.prev_time = self.current_time
+        self.sample_time = s_time # How often to update PID
+
     def PN_control(self, robot_action, robot_target_diff):
+
+        robot_new_action = robot_action # Will be changed once PN is called
         
         # Normalising input vector and old vector diff
         robot_target_diff_unit = robot_target_diff / np.linalg.norm(robot_target_diff)
         robot_target_diff_old_unit = self.robot_target_diff_old / np.linalg.norm(self.robot_target_diff_old)
 
-        # Computing dot product
-        #dot_product = np.dot(robot_target_diff_unit, robot_target_diff_old_unit)
-        a = robot_target_diff_unit
-        b = robot_target_diff_old_unit
-        # Computing angle between unit vectors based on dot product (in degrees)
-        error_angle = -np.arctan2(a[0]*b[1] - a[1]*b[0], a[0]*b[0]+a[1]*b[1])
-        error_angle = self.N * error_angle
+        # Current time when doing PN
+        self.current_time = time.time()
+        delta_time = self.current_time - self.prev_time
 
-        # Creating rotation matrix to rotate robot action
-        rot = np.array([[math.cos(error_angle), -math.sin(error_angle)], [math.sin(error_angle), math.cos(error_angle)]])
+        if delta_time >= self.sample_time:
 
-        # Rotating robot action vector
-        robot_new_action = np.dot(rot,robot_action)
+            # Computing dot product
+            a = robot_target_diff_unit
+            b = robot_target_diff_old_unit
+            # Computing angle between unit vectors based on dot product (in degrees)
+            error_angle = -np.arctan2(a[0]*b[1] - a[1]*b[0], a[0]*b[0]+a[1]*b[1])
+            error_angle = self.N * error_angle
 
-        self.robot_target_diff_old = robot_target_diff
+            # Creating rotation matrix to rotate robot action
+            rot = np.array([[math.cos(error_angle), -math.sin(error_angle)], [math.sin(error_angle), math.cos(error_angle)]])
+
+            # Rotating robot action vector
+            robot_new_action = np.dot(rot,robot_action)
+
+            self.robot_target_diff_old = robot_target_diff
+
+            self.prev_time = self.current_time
 
         # Information for plotting later
         robot_action_unit = robot_action / np.linalg.norm(robot_action)
